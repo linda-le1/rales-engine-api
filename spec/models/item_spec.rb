daba1383a7321dd53aa3002d35fd13780230c514
Linda Le <yueling1222@gmail.com>
2020-01-28 21:49:10 -0700

    Add item model with working spec test file

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+    describe 'relationships' do
+        it { should have_many :invoice_items }
+        it { should belong_to :merchant }
+    end
+end
