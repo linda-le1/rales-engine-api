028d3dae35cfed29aa8da320faaaf4be40a3426b
Linda Le <yueling1222@gmail.com>
2020-01-28 22:57:40 -0700

    Add show record with passing test for merchant

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 describe "Merchants" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 5)
+    it "sends a list of merchants" do
+        create_list(:merchant, 5)
 
-    get '/api/v1/merchants'
+        get '/api/v1/merchants'
 
-    expect(response).to be_successful
-  end
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body)
+
+        expect(merchants.count).to eql(5)
+    end
+
+    it "can get one merchant by its id" do
+        id = create(:merchant).id
+
+        get "/api/v1/merchants/#{id}"
+
+        merchant = JSON.parse(response.body)
+
+        expect(response).to be_successful
+        expect(merchant["id"]).to eq(id)
+      end
 end
