39248cc5258e83efc7a6cbcf5327ef144d8a0a37
Linda Le <yueling1222@gmail.com>
2020-01-30 23:00:07 -0700

    Add request.query_parameters to the merchant find controller

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::Merchants::FindController < ApplicationController
-
+    def index
+        merchant_params = request.query_parameters
+        render json: MerchantSerializer.new(Merchant.where(merchant_params))
+    end
 end
+
