186737a5fdbdbe382a01098fbecd265a14822d73
Linda Le <yueling1222@gmail.com>
2020-02-01 21:35:02 -0700

    Add top 3 merchants by revenue in revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
     def index
-
+        MerchantSerializer.new(Merchant.select("merchants.*, sum(unit_price*quantity) as revenue")
+        .joins(invoices: [:invoice_items, :transactions])
+        .where(transactions: {result: 'success'})
+        .group(:id)
+        .order(revenue: :desc)
+        .limit(3))
     end
+
 end
+
+
