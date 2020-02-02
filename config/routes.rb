4389d94627af5f3eb0edd29ab2f0c6f9be05e760
Linda Le <yueling1222@gmail.com>
2020-02-02 13:28:42 -0700

    Add find all route for customers

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
       namespace :customers do
         get '/find', to: 'find#index'
         get '/random', to: 'random#index'
+        get '/find_all', to: 'find#show'
+
       end
 
       namespace :invoice_items do
