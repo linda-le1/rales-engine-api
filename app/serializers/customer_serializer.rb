1de0fc7523b8fb53d6e64838c7bf88702fc2f9a8
Linda Le <yueling1222@gmail.com>
2020-02-02 13:28:33 -0700

    Add dates to customer serializer

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,6 +1,6 @@
 class CustomerSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :first_name, :last_name, :id
+  attributes :first_name, :last_name, :id, :created_at, :updated_at
   has_many :invoices
   has_many :transactions, through: :invoices
 end
