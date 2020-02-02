d879485fb681c7a245bfc6543ac830b9ca8383c3
Linda Le <yueling1222@gmail.com>
2020-02-02 14:19:50 -0700

    Move activerecord query over to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,13 @@ class Merchant < ApplicationRecord
     has_many :invoices, dependent: :destroy
     has_many :transactions, through: :invoices
     has_many :invoice_items, through: :invoices
+
+    def calculate_most_revenue(quantity)
+        select("merchants.*, sum(unit_price*quantity) as revenue")
+        .joins(invoices: [:invoice_items, :transactions])
+        .group(:id)
+        .where(transactions: {result: 'success'})
+        .order(revenue: :desc)
+        .limit(quantity)
+    end
 end
