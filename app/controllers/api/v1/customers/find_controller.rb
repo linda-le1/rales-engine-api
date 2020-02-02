2c10a5cc4657bbc5b81f29c978e7eb9333da0888
Linda Le <yueling1222@gmail.com>
2020-02-02 13:28:57 -0700

    Add controller show method for find all customers

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::Customers::FindController < ApplicationController
         render json: CustomerSerializer.new(Customer.find_by(request.query_parameters))
     end
 
+    def show
+        render json: CustomerSerializer.new(Customer.where(request.query_parameters))
+    end
+
 end
