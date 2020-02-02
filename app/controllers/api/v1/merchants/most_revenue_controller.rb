745f21e82560661ed03e7bafb3ca390a9eadb650
Linda Le <yueling1222@gmail.com>
2020-02-02 15:39:12 -0700

    Change previous revenue controller to most revenue and add revenue controller

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,8 +1,9 @@
-class Api::V1::Merchants::RevenueController < ApplicationController
+class Api::V1::Merchants::MostRevenueController < ApplicationController
 
     def index
         render json: MerchantSerializer.new(Merchant.calculate_most_revenue(params[:quantity]))
     end
 
 end
+
 
