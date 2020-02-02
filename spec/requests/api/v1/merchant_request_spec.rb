df41e853247194b58e9ae644bf82a05fbb07a59e
Linda Le <yueling1222@gmail.com>
2020-02-02 15:18:34 -0700

    Add test for total revenue by date

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -247,4 +247,47 @@ describe 'Merchants' do
         expect(top_merchants[2]["attributes"]["id"]).to eq(merchant_3.id)
 
     end
+
+    it 'returns the total revenue for date x across all merchants' do
+        merchant_1 = create(:merchant)
+        merchant_2 = create(:merchant)
+        merchant_3 = create(:merchant)
+
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+
+        item_1 = create(:item, unit_price: 2150, merchant_id: merchant_1.id)
+        item_2 = create(:item, unit_price: 1250, merchant_id: merchant_2.id)
+        item_3 = create(:item, unit_price: 4550, merchant_id: merchant_3.id)
+
+        invoice_1 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id, created_at: "2020-01-31 10:20:30 UTC")
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_1.id, created_at: "2020-01-31 10:20:30 UTC")
+        invoice_3 = create(:invoice, merchant_id: merchant_3.id, customer_id: customer_2.id, created_at: "2020-01-31 10:20:30 UTC")
+        invoice_4 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id, created_at: "2020-01-31 10:20:30 UTC")
+        invoice_5 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id, created_at: "2020-01-31 10:20:30 UTC")
+
+        transaction_1 = create(:transaction, result: 'success', invoice_id: invoice_1.id)
+        transaction_2 = create(:transaction, result: 'success', invoice_id: invoice_2.id)
+        transaction_3 = create(:transaction, result: 'success', invoice_id: invoice_3.id)
+        transaction_4 = create(:transaction, result: 'success', invoice_id: invoice_4.id)
+        transaction_5 = create(:transaction, result: 'failed', invoice_id: invoice_5.id)
+
+        invoice_item_1 = create(:invoice_item, quantity: 2, invoice_id: invoice_1.id, item_id: item_1.id)
+        invoice_item_2 = create(:invoice_item, quantity: 2, invoice_id: invoice_2.id, item_id: item_2.id)
+        invoice_item_3 = create(:invoice_item, quantity: 2, invoice_id: invoice_3.id, item_id: item_3.id)
+        invoice_item_4 = create(:invoice_item, quantity: 2, invoice_id: invoice_4.id, item_id: item_1.id)
+        invoice_item_5 = create(:invoice_item, quantity: 5, invoice_id: invoice_5.id, item_id: item_2.id)
+
+        get '/api/v1/merchants/revenue?date=2020-01-31'
+
+        top_merchants = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(top_merchants.count). to eql(3)
+        expect(top_merchants[0]["attributes"]["id"]).to eq(merchant_1.id)
+        expect(top_merchants[1]["attributes"]["id"]).to eq(merchant_2.id)
+        expect(top_merchants[2]["attributes"]["id"]).to eq(merchant_3.id)
+
+    end
 end
