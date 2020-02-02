67f1ecd69c29c436943772a064146d5ad7530170
Linda Le <yueling1222@gmail.com>
2020-02-02 14:20:13 -0700

    Refactor revenue controller and include quantity params

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,12 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
     def index
-        render json: MerchantSerializer.new(Merchant.select("merchants.*, sum(unit_price*quantity) as revenue")
-        .joins(invoices: [:invoice_items, :transactions])
-        .group(:id)
-        .where(transactions: {result: 'success'})
-        .order(revenue: :desc)
-        .limit(3))
+        render json: MerchantSerializer.new(Merchant.calculate_most_revenue(params[:quantity]))
     end
 
 end
