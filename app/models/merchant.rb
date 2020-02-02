5a0986d731e978e4c6bfdb986fa8a2761c1dd2f1
Linda Le <yueling1222@gmail.com>
2020-02-02 16:22:29 -0700

    Add scope for parsed date and add merge into revenue by date method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,8 @@ class Merchant < ApplicationRecord
     has_many :transactions, through: :invoices
     has_many :invoice_items, through: :invoices
 
+    scope :parse_date, -> (date) { where(parse_date(date))}
+
     def self.calculate_most_revenue(quantity)
         select("merchants.*, sum(unit_price*quantity) as revenue")
         .joins(invoices: [:invoice_items, :transactions])
@@ -15,11 +17,11 @@ class Merchant < ApplicationRecord
 
     def self.calculate_revenue_by_date(date)
         select("merchant.name, invoice.created_at as date, sum(invoice_items.quantity * invoice_items.unit_price) as revenue")
-        .joins([:invoices [:invoice_items, :transactions]])
-        .group(merchant.name, "date")
+        .joins(invoices: [:invoice_items, :transactions])
+        .group(:name, "date")
+        .merge(Invoice.parse_date(date))
         .where(transaction {result:"success"})
         .order('revenue desc')
-        .where(Invoice.parse_date(date))
     end
 
     def parse_date(date)
