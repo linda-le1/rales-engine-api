f7fc16db18cebcd59806ec5ab259dab5cad3feee
Linda Le <yueling1222@gmail.com>
2020-02-01 21:08:51 -0700

    Add set up for merchants by revenue test

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -202,7 +202,39 @@ describe 'Merchants' do
     end
 
     it 'can find the top x merchants ranked by total revenue' do
-        
+        merchant_1 = create(:merchant)
+        merchant_2 = create(:merchant)
+        merchant_3 = create(:merchant)
+        merchant_4 = create(:merchant)
+        merchant_5 = create(:merchant)
+
+        customer_1 = create(:customer)
+        customer_2 = create(:customer)
+
+        item_1 = create(:item, unit_price: 50, merchant_id: merchant_1.id)
+        item_2 = create(:item, unit_price: 40, merchant_id: merchant_2.id)
+        item_3 = create(:item, unit_price: 30, merchant_id: merchant_3.id)
+        item_4 = create(:item, unit_price: 20, merchant_id: merchant_4.id)
+        item_4 = create(:item, unit_price: 100, merchant_id: merchant_5.id)
+
+        invoice_item_1 = create(:invoice_item, quantity: 2, invoice_id: invoice_1.id, item_id: item_1.id)
+        invoice_item_2 = create(:invoice_item, quantity: 2, invoice_id: invoice_2.id, item_id: item_2.id)
+        invoice_item_3 = create(:invoice_item, quantity: 2, invoice_id: invoice_3.id, item_id: item_3.id)
+        invoice_item_4 = create(:invoice_item, quantity: 2, invoice_id: invoice_4.id, item_id: item_4.id)
+        invoice_item_5 = create(:invoice_item, quantity: 5, invoice_id: invoice_5.id, item_id: item_5.id)
+
+        invoice_1 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_1.id)
+        invoice_3 = create(:invoice, merchant_id: merchant_3.id, customer_id: customer_2.id)
+        invoice_4 = create(:invoice, merchant_id: merchant_4.id, customer_id: customer_1.id)
+        invoice_5 = create(:invoice, merchant_id: merchant_5.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, result: 'success', invoice_id: invoice_1.id)
+        transaction_2 = create(:transaction, result: 'success', invoice_id: invoice_2.id)
+        transaction_3 = create(:transaction, result: 'success', invoice_id: invoice_3.id)
+        transaction_4 = create(:transaction, result: 'success', invoice_id: invoice_4.id)
+        transaction_5 = create(:transaction, result: 'failed', invoice_id: invoice_5.id)
+
 
     end
 end
