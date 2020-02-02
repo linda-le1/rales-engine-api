600b17d31f442bbf39e33265358b342c782cb161
Linda Le <yueling1222@gmail.com>
2020-02-02 11:40:41 -0700

    Add multi finders test for transactions

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -209,4 +209,166 @@ describe 'Transactions' do
         expect(ids).to include(random_transaction['data']['attributes']['id'])
     end
 
+    it 'can find all transactions by an id' do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+
+        customer = create(:customer)
+        customer_2 = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, credit_card_number: '1111', invoice_id: invoice.id, id: 888)
+        transaction_2 = create(:transaction, invoice_id: invoice_2.id, id: 1234)
+
+        get "/api/v1/transactions/find_all?id=#{transaction_1.id}"
+
+
+        transactions = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(transactions.count).to eql(1)
+
+        expect(transactions[0]['attributes']['id']).to eql(transaction_1.id)
+    end
+
+    it 'can find all transactions by an invoice id' do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+
+        customer = create(:customer)
+        customer_2 = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, credit_card_number: '1111', invoice_id: invoice.id)
+        transaction_2 = create(:transaction, invoice_id: invoice.id)
+
+        get "/api/v1/transactions/find_all?invoice_id=#{transaction_1.invoice_id}"
+
+
+        transactions = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(transactions.count).to eql(2)
+
+        expect(transactions[0]['attributes']['id']).to eql(transaction_1.id)
+
+        expect(transactions[1]['attributes']['id']).to eql(transaction_2.id)
+    end
+
+    it 'can find all transactions by credit card number' do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+
+        customer = create(:customer)
+        customer_2 = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, credit_card_number: '1111', invoice_id: invoice.id)
+        transaction_2 = create(:transaction, credit_card_number: '1111', invoice_id: invoice_2.id)
+
+        get "/api/v1/transactions/find_all?credit_card_number=#{transaction_1.credit_card_number}"
+
+
+        transactions = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(transactions.count).to eql(2)
+
+        expect(transactions[0]['attributes']['id']).to eql(transaction_1.id)
+
+        expect(transactions[1]['attributes']['id']).to eql(transaction_2.id)
+    end
+
+    it 'can find all transactions by status' do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+
+        customer = create(:customer)
+        customer_2 = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+        invoice_3 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, credit_card_number: '88881234', invoice_id: invoice.id, result: 'success')
+        transaction_2 = create(:transaction, invoice_id: invoice_2.id, result: 'success')
+        transaction_3 = create(:transaction, invoice_id: invoice_2.id, result: 'failed')
+
+        get "/api/v1/transactions/find_all?result=success"
+
+        transactions = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(transactions.count).to eql(2)
+
+        expect(transactions[0]['attributes']['id']).to eql(transaction_1.id)
+
+        expect(transactions[1]['attributes']['id']).to eql(transaction_2.id)
+    end
+
+    it 'can find all transactions by date created' do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+
+        customer = create(:customer)
+        customer_2 = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+        invoice_3 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, invoice_id: invoice.id, created_at: "2019-01-31 01:30:08 UTC")
+        transaction_2 = create(:transaction, invoice_id: invoice_2.id, created_at: "2019-01-31 01:30:08 UTC")
+        transaction_3 = create(:transaction, invoice_id: invoice_3.id, created_at: "2001-04-11 01:30:08 UTC")
+
+        get "/api/v1/transactions/find_all?created_at=2019-01-31 01:30:08 UTC"
+
+
+        transactions = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(transactions.count).to eql(2)
+
+        expect(transactions[0]['attributes']['id']).to eql(transaction_1.id)
+
+        expect(transactions[1]['attributes']['id']).to eql(transaction_2.id)
+    end
+
+    it 'can find a specific transaction by date updated' do
+        merchant = create(:merchant)
+        merchant_2 = create(:merchant)
+
+        customer = create(:customer)
+        customer_2 = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+        invoice_3 = create(:invoice, merchant_id: merchant_2.id, customer_id: customer_2.id)
+
+        transaction_1 = create(:transaction, invoice_id: invoice.id, updated_at: "2019-01-31 01:30:08 UTC")
+        transaction_2 = create(:transaction, invoice_id: invoice_2.id, updated_at: "2019-01-31 01:30:08 UTC")
+        transaction_3 = create(:transaction, invoice_id: invoice_3.id, updated_at: "2001-04-11 01:30:08 UTC")
+
+        get "/api/v1/transactions/find_all?updated_at=2019-01-31 01:30:08 UTC"
+
+        transactions = JSON.parse(response.body)['data']
+
+        expect(transactions.count).to eql(2)
+
+        expect(transactions[0]['attributes']['id']).to eql(transaction_1.id)
+
+        expect(transactions[1]['attributes']['id']).to eql(transaction_2.id)
+    end
+
 end
