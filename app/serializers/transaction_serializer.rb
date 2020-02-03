a7a6f8543fef6825c2272b48fd929dcb0cb2866d
Linda Le <yueling1222@gmail.com>
2020-02-02 19:11:26 -0700

    Remove dates created and updated per spec harness

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,5 +1,5 @@
 class TransactionSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :invoice_id, :credit_card_number, :result, :id, :created_at, :updated_at
+  attributes :invoice_id, :credit_card_number, :result, :id
   belongs_to :invoice
 end
