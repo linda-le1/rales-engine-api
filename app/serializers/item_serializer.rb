dcb2a6057d0ee656b25b33a45a8ac68e384bfa08
Linda Le <yueling1222@gmail.com>
2020-02-02 21:56:02 -0700

    Put unit price back in serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :merchant_id, :unit_price
+  attributes :id, :name, :description, :merchant_id
+
+  attribute :unit_price do |item|
+    '%.2f' % (item.unit_price / 100)
+  end
 
   belongs_to :merchant
   has_many :invoice_items
