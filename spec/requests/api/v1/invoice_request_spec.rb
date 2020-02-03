ff9f8b04a73354a5baaa2578797ee69742f97c40
Linda Le <yueling1222@gmail.com>
2020-02-02 18:48:54 -0700

    Add test to find all invoice items for specific invoice

--- spec/requests/api/v1/invoice_request_spec.rb
+++ spec/requests/api/v1/invoice_request_spec.rb
@@ -293,7 +293,7 @@ describe 'Invoices' do
         expect(invoices[1]['attributes']['id']).to eql(invoice_2.id)
     end
 
-    it 'can find all transactions for an invoice' do
+    it 'can find all transactions belonging to an invoice' do
         merchant = create(:merchant)
 
         customer = create(:customer)
@@ -312,5 +312,30 @@ describe 'Invoices' do
         expect(transactions.count).to eql(5)
         expect(transactions.last['attributes']['invoice_id']).to_not be eql(invoice_2.id)
 
+    end
+
+    it 'can find all invoice items belonging to an invoice' do
+        merchant = create(:merchant)
+
+        customer = create(:customer)
+
+        invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+        invoice_2= create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+
+        item_1 = create(:item, merchant_id: merchant.id)
+        item_2 = create(:item, merchant_id: merchant.id)
+
+        create_list(:invoice_item, 5, invoice_id: invoice.id, item_id: item_1.id)
+        create_list(:invoice_item, 10, invoice_id: invoice_2.id, item_id: item_2.id)
+
+
+        get "/api/v1/invoices/#{invoice.id}/invoice_items"
+
+        invoice_items = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(invoice_items.count).to eql(5)
+        expect(invoice_items.last['attributes']['invoice_id']).to_not be eql(invoice_2.id)
     end
 end
