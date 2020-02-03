e751d6abd40c5a3b6f174f6d33950d2ee693df8f
Linda Le <yueling1222@gmail.com>
2020-02-03 00:04:01 -0700

    Add test to get associated invoices by customer id

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -198,4 +198,25 @@ describe 'Customers' do
         expect(customers[0]['attributes']['id']).to eql(customer_2.id)
         expect(customers[1]['attributes']['id']).to eql(customer_3.id)
     end
+
+    it 'can find all associated invoices by customer id' do
+
+        customer = create(:customer, id: 888)
+
+        merchant = create(:merchant)
+
+        invoices = create_list(:invoice, 5, customer_id: customer.id, merchant_id: merchant.id)
+
+        get "/api/v1/customers/#{customer.id}/invoices"
+
+        invoices = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+
+        expect(invoices.count).to eql(5)
+
+        expect(invoices[0]['attributes']['customer_id']).to eql(customer.id)
+
+
+    end
 end
