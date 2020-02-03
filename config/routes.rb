0e976769743157bda4e6484c2f9496effd54b11b
Linda Le <yueling1222@gmail.com>
2020-02-02 23:46:11 -0700

    Add controller and route to get associated invoice items by item id

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,7 @@ Rails.application.routes.draw do
         get '/find', to: 'find#index'
         get '/random', to: 'random#index'
         get '/find_all', to: 'find#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
       end
 
       namespace :transactions do
